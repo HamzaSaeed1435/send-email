4d2b8edf56b0f7ee395c96a6c9480625795ac040
HamzaSaeed1435 <59760648+HamzaSaeed1435@users.noreply.github.com>
2024-05-19 08:19:15 -0700

    asfsfs

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 30000;
 
 app.use(bodyParser.json());
 app.use(cors());
